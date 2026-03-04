f44942c9f5d4058a3d6f2700e2f7649e6f9fe957
Medhat Attia <medhatsaid669@gmail.com>
2026-03-04 12:13:33 +0000

    Initial commit

---
+++ variables_homework.py
@@ -0,0 +1,28 @@
+# Homework 1: Math operations
+"""
+● Write a program that reads 2 numbers and print their + - * / computations as
+in the picture
+● Do good testing for your code
+○ E.g. consider negative values
+○ E.g. even and odd values
+○ E.g. consider zero as first or 2nd number
+"""
+# first = float(input("Enter first number:\n "))
+#
+# # let's make it in a single line.
+# second = float(input("Enter second number:\n "))
+#
+# print(first, '+', second, '=', first + second )
+# print(first, '-', second, '=', first - second )
+# print(first, '*', second, '=', first * second )
+# print(first, '/', second, '=', first / second )
+#
+# print("\nEnd the programe")
+
+# Homework 2: Students grades
+"""● Write a program that reads 2 students information about math exam
+○ For each student read: his name, id and grade
+● Print the students
+● Print the grades average
+● See the picture"""
+
