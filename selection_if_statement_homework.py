2c0c8f1c2d170ee34cd1b1fdd916b32a3b7cb913
Medhat Attia <medhatsaid669@gmail.com>
2026-03-17 19:14:29 +0000

    new update

---
+++ selection_if_statement_homework.py
@@ -0,0 +1,15 @@
+# Selection Homework 1
+
+# Homework 1: Arithmetic
+
+"""● Read 2 integers A, B and print based on following cases:
+○ if both are odd print their product  A*B
+○ if both are even print their division A/B   (float division / assume B != 0)
+○ if the first is odd and the second is even then find their sum A+B
+○ if the first is even and the second is odd then find their subtraction A-B
+● Inputs ⇒ outputs
+○ 5 7 => 35
+○ 12 2 => 6
+○ 5 6 => 11
+○ 12 3 => 9"""
+
